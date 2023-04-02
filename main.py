3c0d2d79809d54b6aa967e1c6866b189243b9fa8
djsloat <43764827+djsloat@users.noreply.github.com>
2023-04-01 21:00:29 -0700

    Formatting

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from comment_response import Section
 def main():
     with open("config.toml", "rb") as toml:
         config = tomllib.load(toml)
+        
     book = Workbook(config["filename"])
     sheet = book.sheet(config["sheetname"], header_row=config["header_row"])
+
     section = Section(sheet, **config["section"])
     section.write(config["savename"], config["outline_level"])
     section.automark.write(config["automark"])
